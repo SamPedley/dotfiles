8fb28163c95ce2e8b55d7f499b1dfdec4db2f3d1
Sam Pedley <sjpeds@gmail.com>
2018-03-15 20:47:40 -0500

    Remove functions, aliases and packages I won't ever use.

--- brew.sh
+++ brew.sh
@@ -43,59 +43,18 @@ brew install openssh
 brew install screen
 brew install homebrew/php/php56 --with-gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-#brew install aircrack-ng
-#brew install bfg
-#brew install binutils
-#brew install binwalk
-#brew install cifer
-#brew install dex2jar
-#brew install dns2tcp
-#brew install fcrackzip
-#brew install foremost
-#brew install hashpump
-#brew install hydra
-#brew install john
-#brew install knock
-#brew install netpbm
-#brew install nmap
-#brew install pngcheck
-#brew install socat
-#brew install sqlmap
-#brew install tcpflow
-#brew install tcpreplay
-#brew install tcptrace
-#brew install ucspi-tcp # `tcpserver` etc.
-#brew install xpdf
-#brew install xz
-
 # Install other useful binaries.
-brew install ack
-#brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
 brew install rename
-brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
 
-# brew install clojure stuff
-brew cask install java
+# Install other languages
+brew install clojure
 brew install leiningen
+
+brew install python
 
 # Remove outdated versions from the cellar.
 brew cleanup
