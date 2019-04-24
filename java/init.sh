7a7cb8fc7e678fcbf99a274927645866cd95c78a
Sam Pedley <sjpeds@gmail.com>
2019-04-24 12:41:18 -0500

    java version stuff

--- java/init.sh
+++ java/init.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
- 
- brew cask install java
+
+# Install the latest java version
+brew cask install java
+
+# Install open JDK 8,9 & 10
+brew cask install adoptopenjdk8
+brew cask install adoptopenjdk9
+brew cask install adoptopenjdk10
+
+
+# Set the java version to openJDK 1.8
+# Best to keep this in the bas_profile to set as default
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
